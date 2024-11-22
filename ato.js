0925618c8a027cb75e8b945159a24d908bd40924
Asep Suprapto <asepsuprapto.id@gmail.com>
2024-11-22 16:26:05 +0700

    updated

--- ato.js
+++ ato.js
@@ -27,9 +27,29 @@ const dbConnection = async () => {
     host: "127.0.0.1",
     port: "3306",
     user: "root",
-    password: "root",
+    password: "secret",
     database: "atobot",
   });
+};
+
+const billConnection = async () => {
+  return await mysql.createConnection({
+    host: "192.168.0.209",
+    port: "3307",
+    user: "root",
+    password: "pohodeui",
+    database: "pdam_sopp",
+  });
+};
+
+const getSession = async (from) => {
+  const db = await dbConnection();
+  const [rows] = await db.query(
+    "select `id`, `step` from `sessions` where `from` = ? and `expired_at` > unix_timestamp() order by `expired_at` desc limit 1",
+    [from]
+  );
+  if (rows.length > 0) return rows[0];
+  return false;
 };
 
 const storeSession = async (from) => {
@@ -44,18 +64,8 @@ const updateSession = async (from, step) => {
   const db = await dbConnection();
   await db.execute(
     "update `sessions` set `step` = ? where `from` = ? and `expired_at` > unix_timestamp()",
-    [from, step]
+    [step, from]
   );
-};
-
-const getSession = async (from) => {
-  const db = await dbConnection();
-  const [rows] = await db.query(
-    "select `id`, `step` from `sessions` where `from` = ? and `expired_at` > unix_timestamp() order by `expired_at` desc limit 1",
-    [from]
-  );
-  if (rows.length > 0) return rows[0];
-  return false;
 };
 
 const getGreeting = async () => {
@@ -67,13 +77,33 @@ const getGreeting = async () => {
   return false;
 };
 
-const getContext = async (question) => {
+const getKeywords = async (message) => {
   const db = await dbConnection();
   const [rows] = await db.query(
-    "select id, keyword, match (keyword) against (? in natural language mode) as score from keywords where match (keyword) against (? in natural language mode)",
-    [question, question]
+    "select id, keyword, answer_id, match (keyword) against (? in natural language mode) as score from keywords where match (keyword) against (? in natural language mode) limit 2",
+    [message, message]
   );
   if (rows.length > 0) return rows;
+  return false;
+};
+
+const getAnswer = async (id) => {
+  const db = await dbConnection();
+  const [rows] = await db.query(
+    "select `id`, `answer`, `child_id` from `answers` where `id` = ?",
+    [id]
+  );
+  if (rows.length > 0) return rows[0];
+  return false;
+};
+
+const getBill = async (id) => {
+  const db = await billConnection();
+  const [rows] = await db.query(
+    "select `id`, `answer`, `child_id` from `answers` where `id` = ?",
+    [id]
+  );
+  if (rows.length > 0) return rows[0];
   return false;
 };
 
@@ -86,6 +116,7 @@ client.on("qr", (qr) => {
 });
 
 client.on("message", async (message) => {
+
   // const mentions = await message.getMentions();
   // for (let mention of mentions) {
   //   if (mention.isMe) {
@@ -114,33 +145,52 @@ client.on("message", async (message) => {
 
   const session = await getSession(message.from);
 
-  if (message.body === "!bot") {
-    await storeSession(message.from);
-    const greeting = await getGreeting();
+  if (message.body === "!ping") {
     const chat = await message.getChat();
     chat.sendStateTyping();
-
+    setTimeout(() => {
+      chat.clearState();
+      client.sendMessage(message.from, "pong!");
+    }, 3000);
+  } else if (message.body === "!bot") {
+    const chat = await message.getChat();
+    chat.sendStateTyping();
+    const greeting = await getGreeting();
+    await storeSession(message.from);
     setTimeout(() => {
       chat.clearState();
       client.sendMessage(message.from, greeting.message);
-    }, 5000);
+    }, 3000);
   } else if (session && session.step == 0) {
-    const context = await getContext(message.body);
     const chat = await message.getChat();
     chat.sendStateTyping();
+    const keywords = await getKeywords(message.body);
+    let reply = "";
+    if (keywords) {
+      let score_1 = (keywords[0]) ? keywords[0].score : 0;
+      let score_2 = (keywords[1]) ? keywords[1].score : 0;
+      if (score_1 == score_2) {
+        reply = "Saya tidak memahami konteks dari pertanyaan tersebut";
+      } else {
+        if (keywords[0].answer_id) {
+          const answer = await getAnswer(keywords[0].answer_id);
+          reply = answer.answer;
+          await updateSession(message.from, 1);
+        } else {
+          reply = "Mohon maaf, informasi mengenai hal tersebut belum bisa Saya sampaikan";
+        }
+      }
+    } else if (session && session.step == 1) {
 
-    let answer = "";
-
-    if (context) {
-      answer = "data ketemu";
     } else {
-      answer = "tidak ada";
+      reply = "Mohon maaf, Saya belum bisa menjawab pertanyaan tersebut";
     }
-
     setTimeout(() => {
       chat.clearState();
-      client.sendMessage(message.from, answer);
-    }, 5000);
+      if (reply) {
+        client.sendMessage(message.from, reply);
+      }
+    }, 3000);
   }
 });
 
